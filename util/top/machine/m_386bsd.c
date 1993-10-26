c8194266c712cc0fb75463269b0f23892392f8e5
nate <nate@FreeBSD.org>
1993-10-26 21:01:33 +0000

    Updated 'top' to know include the percentage of swap used. (yay!) based on patches from Wolfram Schneider <wosch@cs.tu-berlin.de>

--- util/top/machine/m_386bsd.c
+++ util/top/machine/m_386bsd.c
@@ -14,6 +14,8 @@
  *
  * AUTHOR:  Steve Hocking (adapted from Christos Zoulas <christos@ee.cornell.edu>)
  *	    Updated by Andrew Herbert <andrew@werple.apana.org.au>
+ *	    Added swap statistics Wolfram Schneider <wosch@cs.tu-berlin.de>
+ *	    Changed swap format Nate Williams <nate@bsd.coe.montana.edu>
  */
 
 #include <sys/types.h>
@@ -175,14 +177,14 @@ char *cpustatenames[] = {
 
 /* these are for detailing the memory statistics */
 
-int memory_stats[8];
+int memory_stats[10];
 char *memorynames[] = {
 #ifdef	TOTAL_WORKING
     "Real: ", "K/", "K ", "Virt: ", "K/",
     "K ", "Free: ", "K", NULL
 #else
-    " Free: ", "K ", " Active: ", "K ", " Inactive: ",
-    "K ", " Wired: ", "K ", NULL
+    " Free: ", "K ", " Act: ", "K ", " Inact: ",
+    "K ", " Wired: ", "K ", " Swap: ", "% ",  NULL
 #endif
 };
 
@@ -353,7 +355,7 @@ struct system_info *si;
 	memory_stats[6] = -1;
 	memory_stats[7] = pagetok(total.t_free);
 #else
-        static int free, active, inactive, wired;
+        static int free, active, inactive, wired, swap;
 
 	(void) getkval(nlst[X_PG_FREE].n_value, (int *)(&free), sizeof(free),
                 nlst[X_PG_FREE].n_name);
@@ -371,6 +373,11 @@ struct system_info *si;
 	memory_stats[5] = pagetok(inactive);
 	memory_stats[6] = -1;
 	memory_stats[7] = pagetok(wired);
+	memory_stats[8] = -1;
+	memory_stats[9] = swap_used();
+	if (memory_stats[9] < 0)  /* ignore errors */
+	  memory_stats[9] = 0;
+
 #endif
     }
 
@@ -697,4 +704,160 @@ int pid;
 	}
     }
     return(-1);
+}
+
+/* 
+ * Code from swapinfo(1) 
+ */
+
+/* 
+ * swapinfo
+ *
+ * Swapinfo will provide some information about the state of the swap
+ * space for the system.  It'll determine the number of swap areas,
+ * their original size, and their utilization.
+ *
+ * Kevin Lahey, February 16, 1993
+ */
+
+/* #include <stdlib.h> */
+#include <sys/ioctl.h>
+#include <sys/termios.h>
+#include <sys/stat.h>
+#include <sys/tty.h>
+#include <sys/uio.h>
+#include <sys/buf.h>
+#include <sys/conf.h>
+#include <sys/rlist.h>  /* swapmap defined here... */
+
+
+static struct nlist nl[] = {{"_swapmap"},  /* list of free swap areas */
+#define VM_SWAPMAP	0
+			    {"_swdevt"},   /* list of swap devices and sizes */
+#define VM_SWDEVT	1
+			    {"_nswap"},    /* size of largest swap device */
+#define VM_NSWAP	2
+			    {"_nswdev"},   /* number of swap devices */
+#define VM_NSWDEV	3
+			    {"_dmmax"},    /* maximum size of a swap block */
+#define VM_DMMAX	4
+			    {""}};
+
+int swap_used()
+{
+	int	i, total_avail, total_free, total_partitions, *by_device, 
+	        total_used,
+		nswap, nswdev, dmmax, ch;
+	struct swdevt	*swdevt;
+	struct rlist	head;
+	static long blocksize;
+	int percent_used;
+
+
+	/* Open up /dev/kmem for reading. */
+	
+	if (kvm_openfiles (NULL, NULL, NULL) == -1) 
+	  return(-1);
+
+	/* Figure out the offset of the various structures we'll need. */
+
+	if (kvm_nlist (nl) == -1) 
+	  return(-1);
+
+	if (kvm_read ((void *)nl [VM_NSWAP].n_value, &nswap, 
+		      sizeof (nswap)) != sizeof (nswap)) 
+	  return(-1);
+
+	if (kvm_read ((void *)nl [VM_NSWDEV].n_value, &nswdev, 
+		      sizeof (nswdev)) !=  sizeof (nswdev)) 
+	  return(-1);
+
+	if (kvm_read ((void *)nl [VM_DMMAX].n_value, &dmmax, 
+		      sizeof (dmmax)) != sizeof (dmmax)) 
+	  return(-1);
+
+
+	if ((swdevt = (void *)malloc (sizeof (struct swdevt) * nswdev)) == 
+	    NULL || 
+	    (by_device = (void *)calloc (sizeof (*by_device), nswdev)) == 
+	    NULL) 
+	  return(-1);
+
+	if (kvm_read ((void *)nl [VM_SWDEVT].n_value, swdevt, 
+		      sizeof (struct swdevt) * nswdev) !=
+	    sizeof (struct swdevt) * nswdev) 
+	  return(-1);
+
+	if (kvm_read ((void *)nl [0].n_value, &swapmap, 
+		      sizeof (struct rlist *)) != sizeof (struct rlist *)) 
+	  return(-1);
+
+	/* Traverse the list of free swap space... */
+
+	total_free = 0;
+    	while (swapmap) {
+		int	top, bottom, next_block;
+
+		if (kvm_read ((void *) swapmap, &head, sizeof (struct rlist )) 
+		    != sizeof (struct rlist )) 
+		  return(-1);
+
+		top = head.rl_end;
+		bottom = head.rl_start;
+
+		total_free += top - bottom + 1;
+
+		/*
+		 * Swap space is split up among the configured disk.
+		 * The first dmmax blocks of swap space some from the
+		 * first disk, the next dmmax blocks from the next, 
+		 * and so on.  The list of free space joins adjacent
+		 * free blocks, ignoring device boundries.  If we want
+		 * to keep track of this information per device, we'll
+		 * just have to extract it ourselves.
+		 */
+
+		while (top / dmmax != bottom / dmmax) {
+			next_block = ((bottom + dmmax) / dmmax);
+			by_device [(bottom / dmmax) % nswdev] +=
+				next_block * dmmax - bottom;
+			bottom = next_block * dmmax;
+		}
+
+		by_device [(bottom / dmmax) % nswdev] +=
+			top - bottom + 1;
+
+		swapmap = head.rl_next;
+	}
+
+/*	header = getbsize("swapinfo", &headerlen, &blocksize); */
+	blocksize = 1024; /* may be wrong */
+
+	total_used = 0;
+	for (total_avail = total_partitions = i = 0; i < nswdev; i++) {
+
+		/*
+		 * Don't report statistics for partitions which have not
+		 * yet been activated via swapon(8).
+		 */
+
+		if (!swdevt [i].sw_freed) {
+		  /* printf (" *** not available for swapping ***\n"); */
+		} else {
+		  total_partitions++;
+		  total_avail += swdevt [i].sw_nblks/ (blocksize/512);
+		  total_used += (swdevt [i].sw_nblks -
+			by_device [i]) / (blocksize/512);
+
+		}
+	}
+	if ( total_avail == 0 )
+		return(0);
+	else
+		return((int)100 * total_used/total_avail);
+
+	/* 
+	 * If only one partition has been set up via swapon(8), we don't
+	 * need to bother with totals.
+	 */
 }
