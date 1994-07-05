1089cf79b6ad2d15c38ad428985f8bb84ba31cf8
ache <ache@FreeBSD.org>
1994-07-05 13:31:07 +0000

    Pre-configure this stuff for FreeBSD

---
+++ net/fspclient/config.h
@@ -0,0 +1,79 @@
+/* config.h.  Generated automatically by configure.  */
+/* Generated automatically from configure.in by autoheader.  DO NOT EDIT!  */
+
+/* Define if you have dirent.h.  */
+#define DIRENT 1
+
+/* Define if sys/types.h defines the fd_set structure. */
+#define FD_SET_IN_SYS_TYPES_H 1
+
+/* Define if sys/select.h defines the fd_set structure. */
+#undef FD_SET_IN_SYS_SELECT_H
+
+/* Define if sys/inet.h defines the fd_set structure. */
+#undef FD_SET_IN_SYS_INET_H
+
+/* Define if you have the flock() system call. */
+#define HAVE_FLOCK 1
+
+/* Define if you have the lockf() function. */
+#undef HAVE_LOCKF
+
+/* Define if you have the shmget function, and there is OS support for it.  */
+#undef HAVE_SHMGET
+
+/* Define if you have unistd.h.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if utime.h exists. */
+#define HAVE_UTIME_H 1
+
+/* Define if utime.h (utime()) doesn't exist, but utimes.h (utimes())does. */
+#undef HAVE_UTIMES_H
+
+/* Define if you have vfork.h.  */
+#undef HAVE_VFORK_H
+
+/* Define vfork as fork if vfork does not work.  */
+#undef vfork
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you don't have dirent.h, but have sys/dir.h.  */
+#undef SYSDIR
+
+/* Define if you don't have dirent.h, but have sys/ndir.h.  */
+#undef SYSNDIR
+
+/* Define if the closedir function returns void instead of int.  */
+#undef VOID_CLOSEDIR
+
+/* Define if on AIX 3.  */
+#undef _ALL_SOURCE
+
+/* Define to empty if the keyword does not work.  */
+#undef const
+#undef inline
+
+/* Define if the compiler can cope with ANSI style function prototypes.  */
+#define ANSI_PROTOTYPES 1
+
+/* Define if the compiler cannot cope with ANSI style function
+   prototypes in structure definitions.  */
+#undef BROKEN_STRUCT_PROTOTYPES
+
+/* Define if you have strdup.  */
+#define HAVE_STRDUP 1
+
+/* Define if you have putenv.  */
+#define HAVE_PUTENV 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <memory.h> header file.  */
+#define HAVE_MEMORY_H 1
